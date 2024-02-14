c51ceafe95f63a5829572176206a4588a64e8bc1
Yeabsira Moges <mogesya@gmail.com>
2024-02-13 22:25:46 -0800

    Deletes 14 days old query

--- jobhunter.py
+++ jobhunter.py
@@ -1,3 +1,5 @@
+import datetime
+
 import mysql.connector
 import time
 import json
@@ -7,7 +9,6 @@ import html2text
 
 
 # Connect to database
-# You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running or it will not connect
 def connect_to_sql():
     conn = mysql.connector.connect(user='root', password='',
                                    host='127.0.0.1', database='cne340')
@@ -24,9 +25,7 @@ def create_tables(cursor):
 
 
 # Query the database.
-# You should not need to edit anything in this function
 def query_sql(cursor, query):
-    # print("Executing SQL query:", query)
     cursor.execute(query)
     return cursor
 
@@ -36,10 +35,16 @@ def add_new_job(cursor, jobdetails):
     # extract all required columns
     description = html2text.html2text(jobdetails['description'])
     date = jobdetails['publication_date'][0:10]
-    query = cursor.execute("INSERT INTO jobs( Description, Created_at " ") "
-               "VALUES(%s,%s)", (  description, date))
+    job__id = jobdetails['id']
+    job_comp = jobdetails['company_name']
+    job_url = jobdetails['url']
+    job_title = jobdetails['title']
+    query = cursor.execute("INSERT INTO jobs( Job_id, company, Created_at, url, Title, Description) "
+               "VALUES(%s,%s,%s,%s,%s,%s)", ( job__id, job_comp, date, job_url, job_title, description))
      # %s is what is needed for Mysqlconnector as SQLite3 uses ? the Mysqlconnector uses %s
-    cursor.execute(query, (description, date))
+    delete_14days_query = f'DELETE FROM jobs WHERE created_at < DATE_SUB(NOW(), INTERVAL 14 DAY)'
+    cursor.execute(query,(job__id, job_comp, date, job_url, job_title, description))
+    cursor.execute(delete_14days_query)
     return cursor
 
 
@@ -70,48 +75,40 @@ def fetch_new_jobs():
 
 # Main area of the code. Should not need to edit
 def jobhunt(cursor):
-    # Fetch jobs from website
+
     jobpage = fetch_new_jobs()  # Gets API website and holds the json data in it as a list
-    # use below print statement to view list in json format
-    # print(jobpage)
+
     add_or_delete_job(jobpage, cursor)
 
 
 def add_or_delete_job(jobpage, cursor):
-    # Add your code here to parse the job page
+
     for jobdetails in jobpage['jobs']:  # EXTRACTS EACH JOB FROM THE JOB LIST. It errored out until I specified jobs. This is because it needs to look at the jobs dictionary from the API. https://careerkarma.com/blog/python-typeerror-int-object-is-not-iterable/
-        # Add in your code here to check if the job already exists in the DB
         check_if_job_exists(cursor, jobdetails)
         is_job_found = len(
         cursor.fetchall()) > 0  # https://stackoverflow.com/questions/2511679/python-number-of-rows-affected-by-cursor-executeselect
         if is_job_found:
             delete_job(cursor, jobdetails)
-            print(f'Existing Jobs found in DB {jobdetails["id"]}')
-
+            print(f'Existing Jobs found in DB')
         else:
             add_new_job(cursor, jobdetails)
-            print(f'New Jobs added to DB {jobdetails["description"]}')
+            print(f'New Job found : {jobdetails["title"]}')
             # INSERT JOB
             # Add in your code here to notify the user of a new posting. This code will notify the new user
 
 
 
-# Setup portion of the program. Take arguments and set up the script
-# You should not need to edit anything here.
+
 def main():
-    # Important, rest are supporting functions
-    # Connect to SQL and get cursor
+
     conn = connect_to_sql()
     cursor = conn.cursor()
     create_tables(cursor)
-    # jobhunt(cursor)
+
     while (1):  # Infinite Loops. Only way to kill it is to crash or manually crash it. We did this as a background process/passive scraper
         jobhunt(cursor)
-        time.sleep(10)  # Sleep for 1h, this is ran every hour because API or web interfaces have request limits. Your reqest will get blocked.
+        time.sleep(14400)  # 14400 Sleep for 4h, this is ran every four hour because API or web interfaces have request limits. Your reqest will get blocked.
 
-
-# Sleep does a rough cycle count, system is not entirely accurate
-# If you want to test if script works change time.sleep() to 10 seconds and delete your table in MySQL
 if __name__ == '__main__':
     main()
 
